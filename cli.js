5b68f7084288f4987633d9064060f888e9ce241b
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-10-13 00:43:28 +0300

    Removes the unused files, organizes the code

---
+++ cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+'use strict';
+
+var meow = require('meow');
+var uglifyFolder = require('./');
+
+var cli = meow({
+  help: [
+    'Usage',
+    '  uglifyjs-folder path [options]',
+    '',
+    'options:',
+    '  -c --comments      Will add a comment with the file name.',
+    '  -o --output        Specify a file/folder to write the minified code',
+    '  -e --each          Will minify each file in <output>/*.min.js',
+    '  -h --help          Print this list and exit.'
+  ].join('\n')
+});
+
+
+var res = uglifyFolder(cli.input[0], {
+  comments: cli.flags.comments || cli.flags.c || false,
+  output: cli.flags.output || cli.flags.o,
+  each: cli.flags.each || cli.flags.e || false
+});
+
+if(res) {
+  console.log(res);
+}
+
+
---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var uglifyJS = require('uglify-js');
+var readDir = require('readdir');
+var path = require('path');
+var extend = require('extend');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+var _default = {
+  comments: true,
+  output: '',
+  each: false
+};
+
+module.exports = function(dirPath, options) {
+  var res = '';
+  var opt = extend({}, _default, options);
+
+  // grab and minify all the js files
+  var files = readDir.readSync(dirPath, ['**.js']);
+
+  files.forEach(function(v, i) {
+    var code, newName;
+
+    // minify each file in the new folder
+    if (opt.each) {
+      code = uglifyJS.minify(path.join(dirPath, v)).code;
+
+      // build the new path
+      if(typeof opt.output != 'string') {
+        opt.output = '_out_';
+      }
+      newName = path.join(opt.output,
+        path.dirname(v), path.basename(v, path.extname(v))) + '.min.js';
+
+      mkdirp.sync(path.dirname(newName));
+
+      fs.writeFile(newName, code, function(err) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }
+        console.log('File ' + newName + ' written successfully !');
+      });
+
+    } else {
+      // concatenate all the files into one
+      if (opt.comments) {
+        res += '/**** ' + v + ' ****/\n';
+      }
+
+      res += uglifyJS.minify(path.join(dirPath, v)).code + '\n';
+    }
+
+  });
+
+  // return or write the content in the output file
+  if (opt.output) {
+
+    if (!opt.each) {
+
+      fs.writeFile(opt.output, res, function(err) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }
+        console.log('File ' + opt.output + ' written successfully !');
+      });
+    }
+
+  } else {
+
+    return res;
+  }
+};
